35799390fd54e5de659fe9f8a17496f2aeac652e
Sarah Andrew <sarah.andrew@stu.ocsb.ca>
2023-02-07 22:58:40 +0000

    This program does HelloWorld.

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+* This program executes hello world!
+*
+
+* @author  Sarah Andrew
+* @version 1.0
+
+* @since   2023-02-07
+*/
+
+
+class HelloWorld {
+  public static void main(String[] args) {
+    // prints hello world!
+    System.out.println("Hello Wechie!");
+    System.out.println("We love programming!");
+  }
+}
